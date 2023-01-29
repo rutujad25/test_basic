a2e15e39b5aec3ff06938fa9cd06b4df598db5c5
Rutuja <rbdhokane8@gmail.com>
2023-01-29 14:33:14 +0530

    new hungry code

--- hungry.cpp
+++ hungry.cpp
@@ -2,6 +2,16 @@
 using namespace std;
 int main()
 {
-    cout<<"I am hungry";
+    int samosa;
+
+    cout<<"How many samosa's have u ate?";
+    cin>>samosa;
+    if(samosa==1){
+        cout<<"eat more samosa";
+    }
+    else{
+        cout<<"enough!! do your homework";
+    }
+    
     return 0;
 }
